63677bc1b4a3ea1ea3612869c4036f809235091f
M-sterdeveloper <gubkinivan816@gmail.com>
2022-03-01 19:00:43 +0700

    fix

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -105,7 +105,7 @@ const modals = () => {
     bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
     bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close', true);
     openByScroll('.fixed-gift');
-    // showModalByTime('.popup-consultation', 3000);
+    showModalByTime('.popup-consultation', 3000);
 };
 
 export default modals;
